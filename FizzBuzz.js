fc0939dd96d53c0311cce1c7a70b526ed1e5364c
Alex Padilla <alexpadillaperez@gmail.com>
2020-09-11 23:44:32 -0300

    otra forma de hacer lo mismo

---
+++ FizzBuzz.js
@@ -0,0 +1,22 @@
+// Clase elemental FizzBuzz
+
+class FizzBuzz {
+    // Solo un método para decir si es Fizz o Buzz o ambos
+
+    static isFizzBuzz = (number) => {
+        if((number % 3 === 0) && (number % 5 === 0)) {
+            return "FizzBuzz!"
+        }
+        else if (number % 3 === 0) {
+            return "Fizz"
+        }
+        else if (number % 5 === 0) {
+            return "Buzz"
+        }
+        else {
+            return "what are up to?"
+        }
+    }
+}
+
+console.log(FizzBuzz.isFizzBuzz(5))
